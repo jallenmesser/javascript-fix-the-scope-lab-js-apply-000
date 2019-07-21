72898555bd332d12eead4df6ae690a7cca985e73
Jallen Messersmith <jallenmessersmith@gmail.com>
2019-07-21 18:57:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ function add2(n) {
   return n + two
 }
 
-var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
-  }
+function funkyFunction() {
+    return "FUNKY!
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
